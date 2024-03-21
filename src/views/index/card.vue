cdc11e8d7c74bff2fadca126b5fd246cf00f7432
dnslin <i@dnsl.in>
2024-03-21 11:58:25 +0800

    feat: 增加全局复制按钮

--- src/views/index/card.vue
+++ src/views/index/card.vue
@@ -17,11 +17,28 @@ const props = withDefaults(defineProps<CardProps>(), {
 const newCardInline = ref(props.cardInline);
 const { copyTextToClipboard } = useClipboard();
 
-const copyContent = (content: string) => {
-  copyTextToClipboard(content);
-  message("复制成功", { type: "success" });
-};
-const handleCopyContent = debounce(copyContent, 500, true);
+const handleCopyContent: any = debounce(
+  content => {
+    copyTextToClipboard(content);
+    message("复制成功", { type: "success" });
+  },
+  500,
+  true
+);
+
+const handleCopyAllContent: any = debounce(
+  type => {
+    const content = items.value
+      .filter(item => item.name === type)
+      .map(item => item.content)
+      .join("\n");
+    copyTextToClipboard(content);
+    message("复制成功", { type: "success" });
+  },
+  500,
+  true
+);
+
 function setHoverState(index, state) {
   items.value[index].state = state;
 }
@@ -60,14 +77,27 @@ const items = ref([
 </script>
 
 <template>
-  <div class="dark:text-white dark:bg-black/40 mb-4">
+  <div class="dark:text-white dark:bg-black/40 mb-4 relative w-full h-full">
+    <el-dropdown
+      class="hover:text-cyan-300"
+      style=" position: absolute;top: 0; right: 0"
+    >
+      <span> 全部图片 </span>
+      <template #dropdown>
+        <el-dropdown-men v-for="(item, index) in items" :key="index">
+          <el-dropdown-item @click="handleCopyAllContent(item.name)">{{
+            item.name
+          }}</el-dropdown-item>
+        </el-dropdown-men>
+      </template>
+    </el-dropdown>
     <div
       v-for="(item, index) in items"
       :key="index"
       class="dark:text-white dark:bg-black/40 space-y-2"
     >
       <!-- 适应小屏幕尺寸的文字大小 -->
-      <span class="font-semibold text-lg sm:text-xl">{{ item.name }}</span>
+      <span class="font-medium text-sm sm:text-xl">{{ item.name }}</span>
       <div class="relative w-full mt-2 dark:bg-black/40">
         <p
           class="dark:bg-black/80 dark:text-white scrollable-p text-base sm:text-lg leading-relaxed whitespace-normal sm:whitespace-nowrap select-all mb-2 bg-gray-100 hover:bg-gray-300 text-gray-900 rounded px-2 py-1 cursor-pointer"
@@ -81,10 +111,10 @@ const items = ref([
           sm:width="32px"
           sm:height="32px"
           class="absolute top-1 right-1 flex justify-center items-center cursor-pointer text-xl text-center"
-          :style="{ color: items[index].state ? '#55EA8B' : '#a1d3b3' }"
+          :style="{ color: items[index].state ? '#55EA8B' : '#A3E4BA' }"
           @mouseenter="setHoverState(index, true)"
           @mouseleave="setHoverState(index, false)"
-          @click="handleCopyContent"
+          @click="handleCopyContent(item.content)"
         />
       </div>
     </div>
